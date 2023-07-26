2c19068b62fe441085dd567cbae74de920351d34
Nathan Carter <ncarter@bentley.edu>
2023-07-26 14:37:56 -0400

    Add docs/comments to editor.js and utilities.js

--- editor.js
+++ editor.js
@@ -1,16 +1,34 @@
 
+/**
+ * This file does the load and setup for the TinyMCE editor.  It ensures that
+ * the editor fills the viewable area of the webpage.  It does this upon import,
+ * by loading TinyMCE from its CDN into the page, adding an HTMLElement into
+ * which to install TinyMCE, and then doing that installation.
+ */
+
+// The loadScript function lets us add a script tag to the page, then take some
+// action once that script tag has finished loading its script
 import { loadScript } from './utilities.js'
 
+// TinyMCE's CDN URL, from which we will load it
 const TinyMCEURL = 'https://cdnjs.cloudflare.com/ajax/libs/tinymce/6.6.0/tinymce.min.js'
 
+// Add a textarea input element to the page, into which we will install TinyMCE
+const textarea = document.createElement( 'textarea' )
+textarea.setAttribute( 'id', 'editor' )
+document.body.appendChild( textarea )
+
+// Load TinyMCE from its CDN
 loadScript( TinyMCEURL ).then( () => {
+    // Set up the editor in the textarea we created above
     tinymce.init( {
         selector : '#editor',
-        promotion : false,
+        promotion : false, // disable premium features advertisement
         toolbar : 'undo redo | styles bold italic | alignleft aligncenter alignright outdent indent',
-        plugins : 'fullscreen',
+        plugins : 'fullscreen', // enable full screen mode
         statusbar : false,
         setup : editor => {
+            // Activate full screen mode as soon as the editor is ready
             editor.on( 'init', () => editor.execCommand( 'mceFullScreen' ) )
         }
     } )
--- utilities.js
+++ utilities.js
@@ -1,4 +1,24 @@
 
+/**
+ * A set of generic utility functions used in this project.
+ */
+
+/**
+ * Create a script element to load a script from the given URL, append that
+ * script element to the page's head, and notify us (via a returned `Promise`)
+ * when the script completes loading successfully or fails with an error.
+ * 
+ * Example use:
+ * ```
+ * loadScript( 'https://some.cdn.org/script.js' ).then( () => {
+ *     // Run code that depends on the script having loaded.
+ * } )
+ * ```
+ * 
+ * @param {String} url - URL of the script to load
+ * @returns {Promise} a promise that is resolved if the script finishes loading
+ *   or rejected if the script encounters an error
+ */
 export const loadScript = url => new Promise( ( resolve, reject ) => {
     const scriptTag = document.createElement( 'script' )
     document.head.append( scriptTag )
