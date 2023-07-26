c79e327d0448ca55d6f13838bbd2bb675de174b8
Nathan Carter <ncarter@bentley.edu>
2023-07-26 17:29:31 -0400

    Add copyWithoutPrototype to utilities module

--- utilities.js
+++ utilities.js
@@ -28,3 +28,22 @@ export const loadScript = url => new Promise( ( resolve, reject ) => {
     scriptTag.addEventListener( 'error', reject )
     scriptTag.setAttribute( 'src', url )
 } )
+
+/**
+ * From any JavaScript object, we can create another object by first
+ * constructing a new instance of the Object prototype, then copying over all of
+ * the "own" properties from the original object (by reference).  This is like
+ * the original object, but without its prototype or any other inherited data or
+ * methods.  This function does that.
+ * 
+ * @param {Object} object - any JavaScript object
+ * @returns {Object} a shallow copy of the original object, but without copying
+ *   its prototype information
+ */
+export const copyWithoutPrototype = object => {
+    const result = new Object()
+    for ( let key in object )
+        if ( object.hasOwnProperty( key ) )
+            result[key] = object[key]
+    return result
+}
