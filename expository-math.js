c9228367011d0040852731c1c00df5c07c3c0a04
Ken Monks <ken.monks@gmail.com>
2024-03-20 22:15:40 -0400

    Tweaking the expository math menu item.

--- expository-math.js
+++ expository-math.js
@@ -199,10 +199,15 @@ export const install = editor => {
         atom.update()
         atom.editThenInsert()
     }
+    const svgicon = '<svg width="24" height="24" viewBox="0 0 24 24"><text x="0" y="18" font-size="8" font-family="sans-serif">$$</text></svg>'
+    editor.ui.registry.addIcon('tex',svgicon)
     editor.ui.registry.addMenuItem( 'expositorymath', {
-        text : 'Expository math',
+        // text : 'Expository math',
+        text : 'LaTeX',
         tooltip : 'Insert expository math',
-        shortcut : 'Meta+Alt+E',
+        // shortcut : 'Meta+Alt+E',
+        icon : 'tex',
+        shortcut : '$',
         onAction : insertExpositoryMath
     } )
     // Install that function as what happens when you type a dollar sign,
