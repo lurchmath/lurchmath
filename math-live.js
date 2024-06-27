3d1b7045d0c118182327a32e37c6aabd95c627a2
Ken Monks <ken.monks@gmail.com>
2024-06-27 09:10:53 -0400

    Fix versions of math-live/compute-engine

--- math-live.js
+++ math-live.js
@@ -24,8 +24,8 @@ import { lurchToLatex, latexToLurch, lurchToPutdown } from './parsers/index.js'
 // Ensures the MathLive scripts are loaded, so you can do whatever you want with
 // the stuff they install in the global (window) object thereafter.
 const loadMathFieldClass = () => new Promise( ( resolve, reject ) => {
-    loadScript( 'https://unpkg.com/mathlive' ).then( () => {
-        loadScript( 'https://unpkg.com/@cortex-js/compute-engine' ).then( () => {
+    loadScript( 'https://unpkg.com/mathlive@0.100.0' ).then( () => {
+        loadScript( 'https://unpkg.com/@cortex-js/compute-engine@0.24.1' ).then( () => {
             const tryToResolve = () => {
                 if ( window.MathfieldElement ) {
                     resolve()
@@ -44,7 +44,7 @@ const loadMathFieldClass = () => new Promise( ( resolve, reject ) => {
  * 
  * @type {string}
  */
-export const stylesheet = 'https://unpkg.com/mathlive/dist/mathlive-static.css'
+export const stylesheet = 'https://unpkg.com/mathlive@0.100.0/dist/mathlive-static.css'
 
 /**
  * An item that can be used in a {@link Dialog} and shows up as an equation
